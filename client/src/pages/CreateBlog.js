f601e46e769e51f22bac11cbd37e7d9266605a52
ayush-geek <aymalviya127@gmail.com>
2023-10-08 18:02:13 +0530

    Bug fixed

--- client/src/pages/CreateBlog.js
+++ client/src/pages/CreateBlog.js
@@ -43,81 +43,83 @@ const CreateBlog = () => {
     };
 
     return (
-        <Box
-            sx={{
-                width: "70%",
-                maxWidth: "600px",
-                border: 3,
-                borderRadius: 10,
-                padding: 3,
-                margin: "auto",
-                boxShadow: "10px 10px 20px #ccc",
-                display: "flex",
-                flexDirection: "column",
-                mt: 5,
-                backgroundColor: "#f7f7f7",
-            }}
-        >
-            <Typography
-                variant="h4"
-                textAlign="center"
-                fontWeight="bold"
-                color="#333"
-                mb={3}
+        <form onSubmit={handleSubmit}>
+            <Box
+                sx={{
+                    width: "70%",
+                    maxWidth: "600px",
+                    border: 3,
+                    borderRadius: 10,
+                    padding: 3,
+                    margin: "auto",
+                    boxShadow: "10px 10px 20px #ccc",
+                    display: "flex",
+                    flexDirection: "column",
+                    mt: 5,
+                    backgroundColor: "#f7f7f7",
+                }}
             >
-                Create A Post
-            </Typography>
-            <InputLabel sx={{ fontSize: "16px", fontWeight: "bold" }}>
-                Title
-            </InputLabel>
-            <TextField
-                name="title"
-                value={inputs.title}
-                onChange={handleChange}
-                margin="normal"
-                variant="outlined"
-                required
-            />
-            <InputLabel sx={{ fontSize: "16px", fontWeight: "bold", mt: 2 }}>
-                Description
-            </InputLabel>
-            <TextField
-                name="description"
-                value={inputs.description}
-                onChange={handleChange}
-                margin="normal"
-                variant="outlined"
-                required
-                multiline
-                rows={4}
-            />
-            <InputLabel sx={{ fontSize: "16px", fontWeight: "bold", mt: 2 }}>
-                Image URL
-            </InputLabel>
-            <TextField
-                name="image"
-                value={inputs.image}
-                onChange={handleChange}
-                margin="normal"
-                variant="outlined"
-                required
-            />
-            <InputLabel sx={{ fontSize: "16px", fontWeight: "bold", mt: 2 }}>
-                Tag
-            </InputLabel>
-            <TextField
-                name="tag"
-                value={inputs.tag}
-                onChange={handleChange}
-                margin="normal"
-                variant="outlined"
-            />
-            <Box mt={3} display="flex" justifyContent="center">
-                <Button type="submit" color="primary" variant="contained">
-                    SUBMIT
-                </Button>
+                <Typography
+                    variant="h4"
+                    textAlign="center"
+                    fontWeight="bold"
+                    color="#333"
+                    mb={3}
+                >
+                    Create A Post
+                </Typography>
+                <InputLabel sx={{ fontSize: "16px", fontWeight: "bold" }}>
+                    Title
+                </InputLabel>
+                <TextField
+                    name="title"
+                    value={inputs.title}
+                    onChange={handleChange}
+                    margin="normal"
+                    variant="outlined"
+                    required
+                />
+                <InputLabel sx={{ fontSize: "16px", fontWeight: "bold", mt: 2 }}>
+                    Description
+                </InputLabel>
+                <TextField
+                    name="description"
+                    value={inputs.description}
+                    onChange={handleChange}
+                    margin="normal"
+                    variant="outlined"
+                    required
+                    multiline
+                    rows={4}
+                />
+                <InputLabel sx={{ fontSize: "16px", fontWeight: "bold", mt: 2 }}>
+                    Image URL
+                </InputLabel>
+                <TextField
+                    name="image"
+                    value={inputs.image}
+                    onChange={handleChange}
+                    margin="normal"
+                    variant="outlined"
+                    required
+                />
+                <InputLabel sx={{ fontSize: "16px", fontWeight: "bold", mt: 2 }}>
+                    Tag
+                </InputLabel>
+                <TextField
+                    name="tag"
+                    value={inputs.tag}
+                    onChange={handleChange}
+                    margin="normal"
+                    variant="outlined"
+                />
+                <Box mt={3} display="flex" justifyContent="center">
+                    <Button type="submit" color="primary" variant="contained">
+                        SUBMIT
+                    </Button>
+                </Box>
             </Box>
-        </Box>
+        </form>
     );
 };
 
